29c4f598f4c4ff8d5d110f176118165064fa07f7
YiggidyYi <105264876+YiggidyYi@users.noreply.github.com>
2024-02-12 14:25:39 -0800

    Added Person and simulation classes

---
+++ src/main/model/Person.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Person {
+    private String state;
+
+    public Person() {
+        this.state = "Alive";
+    }
+
+    public String getState() {
+        return state;
+    }
+
+
+}
