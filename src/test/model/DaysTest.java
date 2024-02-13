8cf144c0a06b26868bd78d29a4d0f1902e3d2ab5
YiggidyYi <105264876+YiggidyYi@users.noreply.github.com>
2024-02-13 12:44:41 -0800

    Added a few more methods and started to build tests for methods

---
+++ src/test/model/DaysTest.java
@@ -0,0 +1,17 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DaysTest {
+    private Person p1;
+    private Person p2;
+    private Person p3;
+
+    @BeforeEach
+    void runBefore(){
+
+    }
+    @Test
+
+}
