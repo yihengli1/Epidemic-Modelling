8fc9d80c33f9cb9bc82d0f3f9c85ac6ee22d3f04
YiggidyYi <105264876+YiggidyYi@users.noreply.github.com>
2024-03-13 18:58:56 -0700

    Added a splash screen

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -4,8 +4,9 @@ import model.PopulationDay;
 
 public class Main {
     public static void main(String[] args) {
-        SimulationPanel sp = new SimulationPanel();
+        //SimulationPanel sp = new SimulationPanel();
         //new ConsoleApp();
         //new StatisticsPanel(new PopulationDay("",0,0,0,0,0));
+        new SplashScreenPanel();
     }
 }
---
+++ src/main/ui/SplashScreenPanel.java
@@ -0,0 +1,44 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SplashScreenPanel extends JFrame {
+    JLabel label;
+    ImageIcon image;
+
+    public SplashScreenPanel() {
+        image = new ImageIcon("./src/bio.png"); //Load image
+        Image img = image.getImage();
+        Image newImg = img.getScaledInstance(125, 125, java.awt.Image.SCALE_SMOOTH); //Scale smooth way
+        image = new ImageIcon(newImg);
+
+        label = new JLabel();
+        label.setIcon(image);
+        label.setText("Loading Epidemic Simulator!");
+        label.setFont(new Font("Arial Black", Font.PLAIN, 15));
+        label.setForeground(Color.WHITE);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalTextPosition(JLabel.CENTER); //set text LEFT, CENTER, RIGHT of imageIcon
+        label.setVerticalTextPosition(JLabel.BOTTOM); //set text TOP, DOWN, RIGHT of imageIcon
+
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //exit out of application when hit x
+        this.setResizable(false); //makes impossible to resize frame
+        this.setSize(400, 400); //sets the x-dimension, and y-dimension of frame
+        this.setLayout(new BorderLayout()); //makes all bounds for labels null, standard layout
+        this.getContentPane().setBackground(Color.BLACK);
+        this.add(label);
+        this.setVisible(true);
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        this.dispose();
+        new SimulationPanel();
+
+
+    }
+}
